506b6eb85ccf97d04c4aace38312ca18e1096ce0
mima <shymaamohammed837@gmail.com>
2019-03-28 10:29:05 -0700

    finish project

--- js/javascode.js
+++ js/javascode.js
@@ -3,7 +3,11 @@
 
 var iteration = 0 ; 
 var rAllPossibleInput = ["paper","rock","scissors"] ; 
-var robotInput ; 
+var robotInput ;
+var userInputImage = document.getElementById("userInputImage") ; 
+var robotInputImage = document.getElementById("robotInputImage") ;
+
+
 
 
 
@@ -19,22 +23,19 @@ function gotoplay(){
 	document.getElementById("instructionsPage").style.display = "none";
 	document.getElementById("mygame").style.display ="block";
 }
+
+
+
+
 /**********************************play ************************************/
 function play(userInput)
 {
-	iteration +=1 ; 
-	checkIteration(iteration) ; 
 	var i = Math.floor(Math.random()*3) ; 
-	console.log(i);
 	robotInput = rAllPossibleInput[i]; 
-	console.log(robotInput);
-	showReult(userInput,robotInput) ; 
-	
-	
-	
-	
-	
-	
+	iteration +=1 ; 
+	showReult(userInput,robotInput) ;
+	 
+	 	
 }
 /******************************check iteration*******************/
 function checkIteration(num)
@@ -45,12 +46,14 @@ function checkIteration(num)
 		}
 	else{
 		
+		showFinalResult();
+		
 	}
 }
 /****************************show result section**********************/
 function showReult(userInput , robotInput)
 {
-	setNumbers(checkInput(userInput,robotInput));
+	
 	/*******************set result p **************/
 	document.getElementById("result").innerHTML = checkInput(userInput,robotInput);
 	if(checkInput(userInput,robotInput) =="Lose")
@@ -69,10 +72,77 @@ function showReult(userInput , robotInput)
 	/******************show result section****************************/
 	document.getElementById("hands").style.display = "none"; 
 	document.getElementById("oneRsult").style.display="block";
+	setNumbers(checkInput(userInput,robotInput));
 	checkIteration(iteration);
 	
 	
 }
+
+
+
+/*********************check inputs***********************************/
+function checkInput(userInput , robotInput)
+{
+	if(userInput ==="paper" )
+		{
+			userInputImage.src="images/paperUser.jpg" ; 
+			if(robotInput === "paper")
+				{
+					
+					robotInputImage.src ="images/paperRobot.jpg" ;  
+					return "Draw" ; 
+				}
+			else if(robotInput === "rock")
+				{
+					robotInputImage.src ="images/rockRobot.jpg" ; 
+					return "Win" ; 
+					
+					
+				}
+			else
+				{
+					robotInputImage.src ="images/scissorsRobot.jpg" ; 
+					return "Lose" ;
+				}
+		}/****************************/
+	else if(userInput ==="rock")
+		{
+			userInputImage.src="images/rockUser.jpg" ;
+			if(robotInput  === "rock")
+				{
+					robotInputImage.src ="images/rockRobot.jpg" ; 
+					return "Draw" ; 
+				}
+			else if(robotInput === "paper")
+				{
+					robotInputImage.src ="images/paperRobot.jpg" ; 
+					return "Win" ; 
+				}
+			else{
+				robotInputImage.src ="images/scissorsRobot.jpg" ; 
+				return "Lose";
+			}
+		}/**********************************************/
+	else{
+		userInputImage.src="images/scissorsUser.jpg" ;
+		if(robotInput ==="scissors")
+			{
+				robotInputImage.src ="images/scissorsRobot.jpg" ;
+				return "Draw" ; 
+			}
+		else if (robotInput ==="rock")
+			{
+				robotInputImage.src ="images/rockRobot.jpg" ;
+				return "Lose";  
+			}
+		else{
+			robotInputImage.src ="images/paperRobot.jpg" ; 
+			return "Win";
+		}
+	}
+	
+}/****end of function**************/
+/*****************************************************************/
 /***************remove result section*********/
 function removeResult()
 {
@@ -81,60 +151,38 @@ function removeResult()
 		document.getElementById("oneRsult").style.display="none";
 		document.getElementById("result").innerHTML ="";
 		
-	},600)	
+	},1000)	
 }
 /************************************************/
-/*********************check inputs***********************************/
-function checkInput(userInput , robotInput)
+function showFinalResult()
 {
-	if(userInput ==="paper" )
+	 userScoreNumber = Number(document.getElementById("myscore").innerHTML);
+     robotScoreNumber = Number(document.getElementById("robotscore").innerHTML);
+	if(userScoreNumber > robotScoreNumber)
 		{
-			if(robotInput === "paper")
-				{
-					
-					return "Draw" ; 
-				}
-			else if(robotInput === "rock")
-				{
-					return "Win" ; 
-					
-					
-				}
-			else
-				{
-					return "Lose" ;
-				}
-		}/****************************/
-	else if(userInput ==="rock")
-		{
-			if(robotInput  === "rock")
-				{
-					return "Draw" ; 
-				}
-			else if(robotInput === "paper")
-				{
-					return "Win" ; 
-				}
-			else{
-				return "Lose";
-			}
-		}/**********************************************/
-	else{
-		if(robotInput ==="scissors")
-			{
-				return "Draw" ; 
-			}
-		else if (robotInput ==="rock")
-			{
-				return "Lose";  
-			}
-		else{
-			return "Win";
+			document.getElementById("finalresult").innerHTML="Win"
+			document.getElementById("finalresult").style.color="green" ; 
 		}
-	}
+	else if (robotScoreNumber > userScoreNumber)
+		{
+			document.getElementById("finalresult").innerHTML="Lose" ; 
+			document.getElementById("finalresult").style.color="red" ;
+			
+		}
+	else
+		{
+			document.getElementById("finalresult").innerHTML="Draw" ; 
+			document.getElementById("finalresult").style.color="green" ; 
+		}
 	
-}/****end of function**************/
-/*****************************************************************/
+	 setTimeout(function(){
+	    document.getElementById("conFinalResult").style.display = "block"; 
+		document.getElementById("oneRsult").style.display="none";
+		document.getElementById("result").innerHTML ="";
+		
+	},1200)	
+}
+
 /***************************set numbers *******************************/
 function setNumbers(result){
 	
@@ -162,4 +210,17 @@ function setNumbers(result){
 	}
 	
 }
+/*****************************************************************************************/
+/******************************playAgain************************************************/
+function playAgain()
+{
+	iteration = 0 ; 
+	document.getElementById("myscore").innerHTML = "0" ; 
+	document.getElementById("robotscore").innerHTML = "0" ;
+	document.getElementById("hands").style.display = "block"; 
+	document.getElementById("oneRsult").style.display="none";
+	document.getElementById("conFinalResult").style.display = "none"; 
+	
+}
+
  
